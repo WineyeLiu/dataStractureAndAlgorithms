3fb03814246fbbfc7365b530921cc1cea2c6737c
Wineye Liu <mistwineye@gmail.com>
2018-03-09 01:16:20 +0800

    finish prime number guess

--- c-implements/primeNumGuess.c
+++ c-implements/primeNumGuess.c
@@ -3,7 +3,7 @@
 
 int isPrime(int n)
 {
-    int res = 1
+    int res = 1;
     for (int i = 2; i <= (int)sqrt(n); i++)
     {
         if(n%i == 0) 
@@ -16,10 +16,41 @@ int isPrime(int n)
 }
 
 
-void main()  
+
+int main()  
 {
+    /*
     int firstP = 0, secondP = 0;
     int N = 20;
     int flag = 1;
     while ()
+    */
+    int max;
+    scanf("%d", &max);
+    int a[max/2];
+    int index = 0;
+    for (int i = 1; i <= max; i++)
+    {
+        if (isPrime(i) == 1)
+        {
+            a[index] = i;
+            index ++;
+        }
+    }
+    /* for (int j = 0; j < index; j++)
+    {
+        printf("%d\n", a[j]);
+    } */
+    //printf("isPrime? %d", isPrime(4));
+    
+    int ct = 0;
+    for (int i = 0; i < index - 1; i++)
+    {
+        if (a[i+1] - a[i] == 2)
+        {
+            ct++;
+        }
+    }
+    printf("%d", ct);
+    return 0;
 }
